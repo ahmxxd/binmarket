4d001bb5c1ec5d01670f01eaf75138bb3786e1f1
ahmxxd <83852720+ahmxxd@users.noreply.github.com>
2023-12-11 23:32:56 +0100

    Home page heading customizations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,11 +9,11 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: "USD" | "EUR" | "GBP" | "BDT";
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "NGN";
     notation?: Intl.NumberFormatOptions["notation"];
   } = {}
 ) {
-  const { currency = "USD", notation = "compact" } = options;
+  const { currency = "NGN", notation = "compact" } = options;
 
   const numericPrice = typeof price === "string" ? parseFloat(price) : price;
 
