9640ab40b51f3b5e1ceb5a18a46c8457e8f684c6
ahmxxd <83852720+ahmxxd@users.noreply.github.com>
2023-12-28 17:06:15 +0100

    Finishing line

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,18 +1,20 @@
-import { PRODUCT_CATEGORIES } from "@/config";
-import { getPayloadClient } from "@/get-payload";
 import { getServerSideUser } from "@/lib/payload-utils";
-import { formatPrice } from "@/lib/utils";
-import { Product, ProductFile } from "@/payload-types";
-import { cookies } from "next/headers";
 import Image from "next/image";
-import Link from "next/link";
+import { cookies } from "next/headers";
+import { getPayloadClient } from "@/get-payload";
 import { notFound, redirect } from "next/navigation";
+import { Product, ProductFile, User } from "@/payload-types";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import PaymentStatus from "@/components/PaymentStatus";
 
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined;
   };
 }
+
 const ThankYouPage = async ({ searchParams }: PageProps) => {
   const orderId = searchParams.orderId;
   const nextCookies = cookies();
@@ -49,17 +51,17 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
 
   return (
     <main className="relative lg:min-h-full">
-      <div className=" hidden md:block h-80 overflow-hidden lg:absolute lg:h-full lg: w-1/2 lg:pr-4 xl:pr-12">
+      <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
         <Image
           fill
-          src="checkout-thank-you.jpg"
+          src="/checkout-thank-you.jpg"
           className="h-full w-full object-cover object-center"
           alt="thank you for your order"
         />
       </div>
 
       <div>
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-x24">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
           <div className="lg:col-start-2">
             <p className="text-sm font-medium text-blue-600">
               Order successful
@@ -68,9 +70,10 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
               Thanks for ordering
             </h1>
             {order._isPaid ? (
-              <p className="mt-2 text-base text-muted-foregournd">
-                Your order was processed and ready for collection at your chosen
-                location. We&apos;ve sent your receipt and order details to{" "}
+              <p className="mt-2 text-base text-muted-foreground">
+                Your order was processed and your assets are available to
+                download below. We&apos;ve sent your receipt and order details
+                to{" "}
                 {typeof order.user !== "string" ? (
                   <span className="font-medium text-gray-900">
                     {order.user.email}
@@ -139,7 +142,8 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
                   );
                 })}
               </ul>
-              <div className="space-y-6 border-t border-gray-200 pt-t text-sm font-medium text-muted-foreground">
+
+              <div className="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-muted-foreground">
                 <div className="flex justify-between">
                   <p>Subtotal</p>
                   <p className="text-gray-900">{formatPrice(orderTotal)}</p>
@@ -156,6 +160,12 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
                 </div>
               </div>
 
+              <PaymentStatus
+                isPaid={order._isPaid}
+                orderEmail={(order.user as User).email}
+                orderId={order.id}
+              />
+
               <div className="mt-16 border-t border-gray-200 py-6 text-right">
                 <Link
                   href="/products"
@@ -171,3 +181,5 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
     </main>
   );
 };
+
+export default ThankYouPage;
